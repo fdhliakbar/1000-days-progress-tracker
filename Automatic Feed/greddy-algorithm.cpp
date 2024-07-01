f3869204d8e18e1f4803343554b6cddb6cfaed56
Aozorasama <104522615+fdhliakbar@users.noreply.github.com>
2024-07-01 20:17:28 +0700

    docs: Upload CemaniID

---
+++ Automatic Feed/greddy-algorithm.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int arr[20];
+    int totalCoints, iterations;
+
+
+    cout << "Masukkan jumlah bilangan: ";
+    cin >> totalCoints;
+
+    
+
+    for (int i = 0; i < totalCoints; i++) {
+
+    }
+}
---
+++ Bank Management System/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <fstream>
+
+class Menu;
+
+
+int main() {
+
+
+    return 0;
+}
